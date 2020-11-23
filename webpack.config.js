7d3f818e40fe455a3356790a2be9e94e44da4e86
Tamas Soos <soostamas.hu@gmail.com>
2020-11-23 18:02:33 +0000

    webpack works

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
+  mode: "development",
+  devtool: "inline-source-map",
 
-    entry: {
-        content: './src/app/content.ts',
-        background: './src/app/background.ts',
-        popup: './src/ui/popup.tsx',
-    },
+  entry: {
+    content: "./src/app/content.ts",
+    background: "./src/app/background.ts",
+    popup: "./src/ui/popup.tsx",
+  },
 
-    output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: '[name].js'
-    },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist/js"),
+  },
 
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"]
-    },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
 
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-        ]
-    },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "pdfjs_static", to: "../" }],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
 };
