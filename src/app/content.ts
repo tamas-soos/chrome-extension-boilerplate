571e68c01bb148ac4ea872b930a23587b165f428
Tamas Soos <soostamas.hu@gmail.com>
2020-11-23 16:41:26 +0000

    copying everything

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,10 +1,12 @@
 import start from "./app.js";
 
+console.log("web content script");
+
 chrome.runtime.sendMessage({}, (response) => {
   var checkReady = setInterval(() => {
     if (document.readyState === "complete") {
       clearInterval(checkReady);
-      console.log("We're in the injected content script!");
+      // console.log("We're in the injected content script!");
 
       start();
 
