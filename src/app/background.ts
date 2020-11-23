f816a7aecdfd1c5874e2524f7571e7a1239093f3
Tamas Soos <soostamas.hu@gmail.com>
2020-11-23 17:48:27 +0000

    new folder

--- src/app/background.ts
+++ src/app/background.ts
@@ -3,6 +3,8 @@
 //   sendResponse({});
 // });
 
+console.log("background is running");
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("Background got a message!");
   console.log("message", message);
