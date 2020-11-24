9db422559d830e881fdec8b725d4bc4b6edf8cc3
Tamas Soos <soostamas.hu@gmail.com>
2020-11-24 10:47:21 +0000

    nice

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const GenerateJsonFromJsPlugin = require("generate-json-from-js-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
 
   entry: {
-    content: "./src/app/content.ts",
-    background: "./src/app/background.ts",
-    popup: "./src/ui/popup.tsx",
+    "js/content": "./src/app/content.ts",
+    "js/background": "./src/app/background.ts",
+    "js/popup": "./src/ui/popup.tsx",
   },
 
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist/js"),
+    path: path.resolve(__dirname, "./build"),
   },
 
   resolve: {
@@ -22,17 +24,22 @@ module.exports = {
 
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.tsx?$/, loader: "ts-loader", exclude: /manifest.js/ },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
 
   plugins: [
+    new CleanWebpackPlugin(),
+
     new CopyPlugin({
-      patterns: [{ from: "pdfjs_static", to: "../" }],
-      options: {
-        concurrency: 100,
-      },
+      patterns: [{ from: "./pdfjs_static" }, { from: "./src/public" }],
+      options: { concurrency: 100 },
+    }),
+
+    new GenerateJsonFromJsPlugin({
+      path: "./src/manifest.js",
+      filename: "manifest.json",
     }),
   ],
 };
